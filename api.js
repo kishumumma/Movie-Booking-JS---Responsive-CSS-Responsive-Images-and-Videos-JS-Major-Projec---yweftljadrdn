72f7bc106725ae3a7c6eea75344f76ca48c522e0
sonika kumari <104119291+kishumumma@users.noreply.github.com>
2023-01-16 16:48:27 +0530

    Update api.js

--- api.js
+++ api.js
@@ -28,7 +28,7 @@ const fetchMovieAvailability = async (movieName) => {
             }
             resolve([])
         }, 500)
-    })
+    });
 }
 
 export { fetchMovieList, fetchMovieAvailability }
